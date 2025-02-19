f5ad83ad6dd2ed1006370972dfdba3fde310e5e0
Oliver Fawkes <64199725+fawkeso16@users.noreply.github.com>
2025-02-19 13:30:51 +0000

    Create Main.java
    File to run sender and reciever via threads

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        Thread senderThread = new Thread(()  -> {
+            try{
+                TextSenderThread.main(new String[]{});
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        });
+        
+        Thread receiverThread = new Thread(() -> {
+            try{
+                TextReceiverThread.main(new String[]{}) ;
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        });
+
+        senderThread.start();
+        receiverThread.start();
+    }
+
+    // public void run(){
+    //     System.out.println("running");
+    // }
+}
+    
+
